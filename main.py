3873b1cc16c338a9e2b0ac85b90dcfe9175e726d
dungeonmaster1324 <59605288+dungeonmaster1324@users.noreply.github.com>
2020-01-19 17:13:25 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,15 +3,14 @@ from enemy import Enemy
 from bullet import Bullet
 from spaceship import Spaceship
 import valuables
-
 pygame.init()
+pygame.time.set_timer(valuables.MYEVENTTYPE, 10)
 SIZE = valuables.WIDTH, valuables.HEIGHT
 SCREEN = pygame.display.set_mode(SIZE)
 clock = pygame.time.Clock()
 running = True
 FPS = 60
-
-
+MYEVENTTYPE = 5
 ship = Spaceship()
 
 while running:
@@ -30,18 +29,36 @@ while running:
                 enemy = Enemy()
                 enemy.spawn()
                 enemy.shoot()
+        if event.type == MYEVENTTYPE:
+            print(True)
 
+    if len(valuables.ENEMIES) == 0:
+        for i in range(valuables.COUNT_ENEMIES):
+            enemy = Enemy()
+            enemy.spawn()
+            enemy.shoot()
+    if valuables.COUNT_ENEMIES == 2:
+        valuables.COUNT_ENEMIES = 1
     for enemy in valuables.ENEMIES:
         enemy.draw(SCREEN)
+
     for bullet in valuables.BULLETS:
         bullet.fly()
         bullet.draw(SCREEN)
 
     for bullet in valuables.BULLETS:
         for enemy in valuables.ENEMIES:
-            if bullet.rect.colliderect(enemy.rect):
+            if bullet.rect.colliderect(enemy.hitbox):
                 valuables.ENEMIES.remove(enemy)
                 valuables.BULLETS.remove(bullet)
+                for i in range(valuables.COUNT_ENEMIES):
+                    enemy = Enemy()
+                    enemy.spawn()
+                    enemy.shoot()
+        if bullet.rect.colliderect(ship.rect):
+            running = False
+            print('LOSE')
+
     ship.draw(SCREEN)
     clock.tick(FPS)
     pygame.display.flip()
